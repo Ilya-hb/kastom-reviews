ff09627e65dd2220802137c2c6943e93f24e5490
ilyholu <ilyholu@gmail.com>
2025-04-27 17:27:40 +0300

    MongoDb connect hosted database

--- server/server.js
+++ server/server.js
@@ -1,7 +1,12 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDb } from "./config/db.js";
 
+dotenv.config();
 const app = express();
+app.get("/", (req, res) => {});
 
-app.listen(5000, () => {
-  console.log("Server started at http://localhost:5000 hello");
+app.listen(3000, () => {
+  connectDb();
+  console.log("Server started at http://localhost:3000 hello");
 });
