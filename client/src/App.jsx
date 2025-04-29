6bac9c55a5ba96002793b5cd6a638ed419ecf784
ilyholu <ilyholu@gmail.com>
2025-04-29 21:20:26 +0300

    #frontend react-router installation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,26 @@
+import { Routes, Route } from "react-router";
+import Admin from "./pages/Admin";
+import Login from "./pages/Login";
+
 function App() {
   return (
-    <div className="flex flex-col justify-center items-center h-screen space-y-5">
-      <h1 className="text-4xl text-white text-center">
-        Братан, давай сделаем сайт с системой отзывов про кассиров?
-      </h1>
-      <h2 className="text-xl text-neutral-400">Помнишь, мы хотели?</h2>
-      <a href="tel:+380508843693">
-        <button className="px-6 py-2 bg-logo text-white outline-0 hover:bg-darker-logo transition duration-200 rounded-sm cursor-pointer font-semibold">
-          Погнали попробуем
-        </button>
-      </a>
-      <h3 className="text-sm text-neutral-400">
-        <span className="font-semibold">P.S</span> А мне надо проектик в
-        портфолио
-      </h3>
-    </div>
+    <>
+      <div className="flex flex-col justify-center items-center h-screen space-y-5">
+        <h1 className="text-4xl text-white text-center">
+          Братан, давай сделаем сайт с системой отзывов про кассиров?
+        </h1>
+        <h2 className="text-xl text-neutral-400">Помнишь, мы хотели?</h2>
+        <a href="tel:+380508843693">
+          <button className="px-6 py-2 bg-logo text-white outline-0 hover:bg-darker-logo transition duration-200 rounded-sm cursor-pointer font-semibold">
+            Погнали попробуем
+          </button>
+        </a>
+        <h3 className="text-sm text-neutral-400">
+          <span className="font-semibold">P.S</span> А мне надо проектик в
+          портфолио
+        </h3>
+      </div>
+    </>
   );
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import { Routes, Route } from "react-router";
+import Admin from "./pages/Admin";
+import Login from "./pages/Login";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <App />
+      <Routes>
+        <Route
+          index
+          element={<App />}
+        />
+        <Route
+          path="/login"
+          element={<Login />}
+        >
+          <Route
+            path="/login/admin"
+            element={<Admin />}
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
