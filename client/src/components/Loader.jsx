7fcc944ff5495ff264dbc8126f1229cdd228de6d
ilyholu <ilyholu@gmail.com>
2025-05-08 20:19:36 +0300

    #frontend Loader component

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -1,5 +1,15 @@
 import React from "react";
+import logo from "../assets/logo.png";
 
 export default function Loader() {
-  return <div className="">Loader</div>;
+  return (
+    <div className="h-screen bg-neutral-950 backdrop-blur-lg flex flex-col items-center justify-center ">
+      <img
+        src={logo}
+        className="max-w-[200px] md:max-w-[300px] animate-pulse"
+        alt="Loading..."
+      />
+      {/* <p className="font-bold text-md md:text-xl">Завантаження</p> */}
+    </div>
+  );
 }
--- client/src/pages/EmployeePage.jsx
+++ client/src/pages/EmployeePage.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Employee from "../components/Employee";
+import Loader from "../components/Loader";
 
 export default function EmployeePage() {
   const { id } = useParams();
@@ -21,8 +22,11 @@ export default function EmployeePage() {
     return <div className="text-white">Завантаження...</div>;
   }
   return (
-    <div className="container flex flex-col mx-auto pt-10">
-      <Employee employeeName={employee.employeeName} />
-    </div>
+    <>
+      <Loader />
+      <div className="container flex flex-col mx-auto pt-10">
+        <Employee employeeName={employee.employeeName} />
+      </div>
+    </>
   );
 }
